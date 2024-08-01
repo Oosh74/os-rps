8213d75d1a072c35013a4a8bf85f53b35e2c3a24
Oosh74 <usherjared@yahoo.com>
2024-08-01 19:56:23 -0400

    Add basic game logic and score keeping

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 console.log('hello world');
 
-const getComputerChoice = () => {
+let getComputerChoice = () => {
   let num = Math.floor(Math.random() * (4 - 1) + 1);
+  let choice = '';
 
   if (num === 1) {
-    console.log('Melee');
+    choice = 'Melee';
   } else if (num === 2) {
-    console.log('Ranged');
+    choice = 'Ranged';
   } else {
-    console.log('Magic');
+    choice = 'Magic';
   }
+
+  return (getComputerChoice = choice);
 };
 
 getComputerChoice();
@@ -18,6 +21,7 @@ let humanChoice = () => {
   let choice = prompt('Melee, Ranged, or Magic?').toLowerCase();
 
   if (choice === 'melee' || choice === 'ranged' || choice === 'magic') {
+    choice = choice[0].toUpperCase() + choice.slice(1);
     return (humanChoice = choice);
   } else {
     alert('Invalid choice, try again');
@@ -27,3 +31,36 @@ let humanChoice = () => {
 
 humanChoice();
 console.log('This is the human choice,', humanChoice);
+console.log('This is the computer choice', getComputerChoice);
+
+const playRound = (pChoice, cpuChoice) => {
+  let playerScore = 0;
+  let cpuScore = 0;
+
+  if (pChoice === 'Melee' && cpuChoice === 'Ranged') {
+    console.log('You win! Melee beats Ranged.');
+    playerScore++;
+  } else if (pChoice === 'Melee' && cpuChoice === 'Magic') {
+    console.log('You lose! Magic beats Melee.');
+    cpuScore++;
+  } else if (pChoice === 'Magic' && cpuChoice === 'Ranged') {
+    console.log('You lose! Ranged beats Magic');
+    cpuScore++;
+  } else if (pChoice === 'Magic' && cpuChoice === 'Melee') {
+    console.log('You win! Magic beats melee.');
+    playerScore++;
+  } else if (pChoice === 'Ranged' && cpuChoice === 'Melee') {
+    console.log('You lose! Melee beats ranged.');
+    cpuScore++;
+  } else if (pChoice === 'Ranged' && cpuChoice === 'Magic') {
+    console.log('You win! Ranged beats magic.');
+    playerScore++;
+  } else {
+    console.log("It's a draw!");
+  }
+
+  console.log(`Player Score: ${playerScore}`);
+  console.log(`Computer Score: ${cpuScore}`);
+};
+
+playRound(humanChoice, getComputerChoice);
