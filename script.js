bb183c43f5b2d5273037cf6e36e7c43da044d987
Oosh74 <usherjared@yahoo.com>
2024-08-01 22:18:03 -0400

    Add logic for a full game of 5 rounds

--- script.js
+++ script.js
@@ -1,66 +1,94 @@
-console.log('hello world');
+const playRound = () => {
+  let getComputerChoice = () => {
+    let num = Math.floor(Math.random() * (4 - 1) + 1);
+    console.log(num);
+    let choice = '';
 
-let getComputerChoice = () => {
-  let num = Math.floor(Math.random() * (4 - 1) + 1);
-  let choice = '';
+    if (num === 1) {
+      choice = 'Melee';
+    } else if (num === 2) {
+      choice = 'Ranged';
+    } else {
+      choice = 'Magic';
+    }
 
-  if (num === 1) {
-    choice = 'Melee';
-  } else if (num === 2) {
-    choice = 'Ranged';
+    return (getComputerChoice = choice);
+  };
+
+  let humanChoice = () => {
+    let choice = prompt('Melee, Ranged, or Magic?').toLowerCase();
+
+    if (choice === 'melee' || choice === 'ranged' || choice === 'magic') {
+      choice = choice[0].toUpperCase() + choice.slice(1);
+      return (humanChoice = choice);
+    } else {
+      alert('Invalid choice, try again');
+      humanChoice();
+    }
+  };
+
+  getComputerChoice();
+  humanChoice();
+
+  console.log('This is the human choice,', humanChoice);
+  console.log('This is the computer choice', getComputerChoice);
+
+  if (humanChoice === 'Melee' && getComputerChoice === 'Ranged') {
+    console.log('You win! Melee beats Ranged.');
+    return 'Player';
+  } else if (humanChoice === 'Melee' && getComputerChoice === 'Magic') {
+    console.log('You lose! Magic beats Melee.');
+    return 'Cpu';
+  } else if (humanChoice === 'Magic' && getComputerChoice === 'Ranged') {
+    console.log('You lose! Ranged beats Magic');
+    return 'Cpu';
+  } else if (humanChoice === 'Magic' && getComputerChoice === 'Melee') {
+    console.log('You win! Magic beats melee.');
+    return 'Player';
+  } else if (humanChoice === 'Ranged' && getComputerChoice === 'Melee') {
+    console.log('You lose! Melee beats ranged.');
+    return 'Cpu';
+  } else if (humanChoice === 'Ranged' && getComputerChoice === 'Magic') {
+    console.log('You win! Ranged beats magic.');
+    return 'Player';
   } else {
-    choice = 'Magic';
+    console.log("It's a draw!");
+    return 'Draw';
   }
-
-  return (getComputerChoice = choice);
 };
 
-getComputerChoice();
-
-let humanChoice = () => {
-  let choice = prompt('Melee, Ranged, or Magic?').toLowerCase();
-
-  if (choice === 'melee' || choice === 'ranged' || choice === 'magic') {
-    choice = choice[0].toUpperCase() + choice.slice(1);
-    return (humanChoice = choice);
-  } else {
-    alert('Invalid choice, try again');
-    humanChoice();
-  }
-};
-
-humanChoice();
-console.log('This is the human choice,', humanChoice);
-console.log('This is the computer choice', getComputerChoice);
-
-const playRound = (pChoice, cpuChoice) => {
+const playGame = () => {
   let playerScore = 0;
   let cpuScore = 0;
 
-  if (pChoice === 'Melee' && cpuChoice === 'Ranged') {
-    console.log('You win! Melee beats Ranged.');
-    playerScore++;
-  } else if (pChoice === 'Melee' && cpuChoice === 'Magic') {
-    console.log('You lose! Magic beats Melee.');
-    cpuScore++;
-  } else if (pChoice === 'Magic' && cpuChoice === 'Ranged') {
-    console.log('You lose! Ranged beats Magic');
-    cpuScore++;
-  } else if (pChoice === 'Magic' && cpuChoice === 'Melee') {
-    console.log('You win! Magic beats melee.');
-    playerScore++;
-  } else if (pChoice === 'Ranged' && cpuChoice === 'Melee') {
-    console.log('You lose! Melee beats ranged.');
-    cpuScore++;
-  } else if (pChoice === 'Ranged' && cpuChoice === 'Magic') {
-    console.log('You win! Ranged beats magic.');
-    playerScore++;
-  } else {
-    console.log("It's a draw!");
+  while (playerScore <= 5 || cpuScore <= 5) {
+    let result = playRound();
+
+    if (result === 'Player') {
+      playerScore++;
+    } else if (result === 'Cpu') {
+      cpuScore++;
+    }
+
+    console.log(`Player Score ${playerScore}`);
+    console.log(`Cpu ${cpuScore}`);
+
+    if (playerScore === 5) {
+      console.log('You win!');
+      break;
+    } else if (cpuScore === 5) {
+      console.log('You lose!');
+      break;
+    }
   }
 
-  console.log(`Player Score: ${playerScore}`);
-  console.log(`Computer Score: ${cpuScore}`);
+  const playAgain = confirm('Would you like to play again');
+
+  if (playAgain) {
+    playGame();
+  } else {
+    return console.log('Thank you for playing!');
+  }
 };
 
-playRound(humanChoice, getComputerChoice);
+playGame();
